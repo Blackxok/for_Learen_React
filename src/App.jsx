bd8ef226f4f224d6768cf14d770ede744e01d750
Blackxok <nasridoni1@gmail.com>
2023-11-06 16:31:58 +0500

    cng last

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable react/jsx-no-undef */
 import Title from "./components/Title";
+// import App_head from "./components/App_head";
+import Add_item from "./components/Add_item";
 
 function App() {
   return (
     <>
       <div className="App_container">
         <Title />
+        <div className="App_body">
+          <Add_item />
+        </div>
       </div>
     </>
   );
--- src/components/Add_item.jsx
+++ src/components/Add_item.jsx
@@ -1,10 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import add_item_style from "../styles/add_item_style.module.css";
 
 function Add_item() {
-  return <>
-  <div className={add_item_style.add_1}>Add_item</div>
-  </>;
+  return (
+    <>
+      <div className={add_item_style.additem_container}>
+        {/* <button className={add_item_style.additem_button}>++Add item++</button> */}
+        <div className="con">
+          <a className="but">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Button
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Add_item;
