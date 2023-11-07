e1d35f4795a2bcf876bf4453b90ef431b3f7889d
Blackxok <nasridoni1@gmail.com>
2023-11-07 22:18:10 +0300

    cng

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,24 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import listStyle from "../styles/list.module.css";
 
 function List() {
-// 
+  //
   // Up
   const [listOfItems, setListOfItems] = useState([
     { name: "BMW", price: "50m$" },
     { name: "Tesla", price: "80m$" },
     { name: "Porsche", price: "40m$" },
   ]);
-
+  //
   // Down
   return (
     <>
-      {listOfItems.map((item) => {
+      {listOfItems.map((item, index) => {
         return (
-          <>
-            <h1 className={}>{item.name}</h1>
-            <h3>{item.price}</h3> 
-          </>
+          <div className={listStyle.boxcha} key={index}>
+            <h1 className={listStyle.nameSt}>{item.name}</h1>
+            <h3>{item.price}</h3>
+          </div>
         );
       })}
     </>
